5072519c7553208f7d25fd4f1683189d7cf03cc8
Peter Schilleman <peter@eenengelswoord.nl>
2024-12-04 17:21:07 +0100

    Better CLI UX for `deno task start`

--- main.ts
+++ main.ts
@@ -1,13 +1,46 @@
-// import { puzzle1 } from "./day1/puzzle1.ts";
-// import { puzzle2 } from "./day1/puzzle2.ts";
-// import { puzzle1 } from "./day2/puzzle1.ts";
-// import { puzzle2 } from "./day2/puzzle2.ts";
-// import { puzzle1 } from "./day3/puzzle1.ts";
-// import { puzzle2 } from "./day3/puzzle2.ts";
-import { puzzle1 } from "./day4/puzzle1.ts";
-import { puzzle2 } from "./day4/puzzle2.ts";
+import { parseArgs } from "jsr:@std/cli/parse-args";
+
+const flags = parseArgs(Deno.args, {
+  string: ["day"],
+});
+
+function error(msg :string) {
+  console.log("%c" + msg, "color: red");
+  Deno.exit(-1);
+}
 
 if (import.meta.main) {
-  console.log("Puzzle 1:", puzzle1());
-  console.log("Puzzle 2:", puzzle2());
+
+  let input = (flags.day) as string;
+  if (!input) {
+    input = prompt("Please enter a day (0 - 25) to fetch the puzzle outcome", '') || '';
+  }
+  if (!input) {
+    error('No day specified.');
+  }
+
+  const day = parseInt(input, 10);
+  if (!day || day < 1 || day > 24) {
+    error('Day not valid');
+  }
+
+  let module1, module2
+  try {
+    module1 = await import (`./day${day}/puzzle1.ts`);
+    module2 = await import (`./day${day}/puzzle2.ts`);
+  } catch (_error) {
+    error ('Module not found')
+  }
+
+  console.log('');
+  console.log(`Going to run puzzles for: %cday ${day}`, 'color: cyan');
+
+  const timeStart = performance.now();
+  console.log('Output for puzzle 1', module1.puzzle1());
+  console.log('Output for puzzle 2', module2.puzzle2());
+  const timeEnd = performance.now();
+  console.log()
+  console.log(`Puzzles took: %c${(timeEnd - timeStart)} milliseconds %cin total`, 'color: cyan', 'color: normal')
+
+  Deno.exit(0);
 }
